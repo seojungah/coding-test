90e0a2d9d170e3bbbe0fb44029886c7b8128c366
seo jungah <66346066+seojungah@users.noreply.github.com>
2025-12-31 12:46:12 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.24 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+    
+        answer = n <= 7 ? 1 : (int)Math.Ceiling((double)n / 7);
+        
+        return answer;
+    }
+}
