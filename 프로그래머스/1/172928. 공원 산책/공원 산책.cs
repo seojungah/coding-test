571fdc038a51f1a1f9c24c05df84b06d3fd44851
seo jungah <66346066+seojungah@users.noreply.github.com>
2026-01-19 13:41:11 +0900

    [level 1] Title: 공원 산책, Time: 0.93 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.cs
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cs
@@ -1,103 +1,51 @@
 using System;
 
 public class Solution {
-    
-    public struct Position{
-        public int x;
-        public int y;
-        public int count;
-    }
-    
     public int[] solution(string[] park, string[] routes) {
-        int[] answer = new int[2];
-        
-        //create map
-        int[,] map = new int[park.Length,park[0].Length];
-        
-        for(int y = 0; y < map.GetLength(0); y++){
-            string line = park[y];
-            
-            for(int x = 0; x < map.GetLength(1); x++){
-                
-                if (line[x] == 'S'){
-                    
-                    answer[0] = y;
-                    answer[1] = x;
-                    map[y,x] = 0;
-                    
-                }else if(line[x] == 'X'){
-                    map[y,x] = 1;
-                }else{
-                    map[y,x] = 0;
-                }
+       int r = 0, c = 0;
+        int h = park.Length;
+        int w = park[0].Length;
+
+        for (int i = 0; i < h; i++) {
+            int startIdx = park[i].IndexOf('S');
+            if (startIdx != -1) {
+                r = i;
+                c = startIdx;
+                break;
             }
         }
-        
-        
-        //direction
-        Position[] direction = new Position[routes.Length];
-        
-        for(int i = 0; i < routes.Length; i++){
-            string next = routes[i];
-            
-            Position pos = new Position(){x=0 , y=0, count=0};
-            
-            pos.count = int.Parse(next[2].ToString());
-            
-            switch(next[0]){
-                case 'N':
-                    pos.x = 0;
-                    pos.y = -1;
-                    break;
-                case 'S':
-                    pos.x = 0;
-                    pos.y = 1;
-                    break;
-                case 'W':
-                    pos.x = -1;
-                    pos.y = 0;
-                    break;
-                case 'E':
-                    pos.x = 1;
-                    pos.y = 0;
-                    break;
-            };
-            
-            direction[i] = pos;
-        }
-        
-        //move
-        for (int i = 0; i < direction.Length; i++)
-        {
-            Position pos = direction[i];
-            
-            int nextY = answer[0];
-            int nextX = answer[1];
+
+        foreach (string route in routes) {
+            string[] parts = route.Split(' ');
+            string dir = parts[0];
+            int dist = int.Parse(parts[1]);
+
+            int dr = 0, dc = 0;
+            if (dir == "N") dr = -1;
+            else if (dir == "S") dr = 1;
+            else if (dir == "W") dc = -1;
+            else if (dir == "E") dc = 1;
+
+            int nr = r, nc = c;
             bool canMove = true;
 
-            for (int j = 1; j <= pos.count; j++)
-            {   
-                nextY += pos.y;
-                nextX += pos.x;
-                
-                //갈 수 있는지 체크
-                if (nextY < 0 || nextY >= park.Length || nextX < 0 || nextX >= park[0].Length || map[nextY, nextX] == 1)
-                {
-                        canMove = false;
-                        break;
+            for (int i = 0; i < dist; i++) {
+                nr += dr;
+                nc += dc;
+
+                if (nr < 0 || nr >= h || nc < 0 || nc >= w || park[nr][nc] == 'X') {
+                    canMove = false;
+                    break;
                 }
             }
 
-            //갈 수 있으면 위치 저장
-            if (canMove)
-            {
-                answer[0] = nextY;
-                answer[1] = nextX;
+            if (canMove) {
+                r = nr;
+                c = nc;
             }
         }
-        
-        
-        return answer;
+
+        return new int[] { r, c };
     }
 
 }
