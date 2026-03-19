eaacdac4d0cceb7db17eef2f343dd6cf0c5d997f
seo jungah <66346066+seojungah@users.noreply.github.com>
2026-03-19 17:52:43 +0900

    [level 0] Title: 배열 자르기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -2,10 +2,11 @@ using System;
 
 public class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
+        int Length = num2 - num1 + 1;
         
-        int[] result = new int[num2 - num1 + 1];
+        int[] result = new int[Length];
         
-        Array.Copy(numbers, num1, result, 0, num2 - num1 + 1);
+        Array.Copy(numbers, num1, result, 0, Length);
         
         return result;
     }
