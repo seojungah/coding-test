3fa56f39b857a750348b5bb9c201548a375d20fd
seo jungah <66346066+seojungah@users.noreply.github.com>
2026-03-19 16:11:19 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+            
+    foreach(int h in array){
+        if(h > height){
+           answer++;
+        }
+    }
+        
+        return answer;
+    }
+}
