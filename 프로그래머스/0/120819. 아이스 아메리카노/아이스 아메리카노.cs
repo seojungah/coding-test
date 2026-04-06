64da11daf928e4cd5eaa339dd5acbbb0a43ce58b
seo jungah <66346066+seojungah@users.noreply.github.com>
2026-04-06 10:02:13 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.26 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        
+        answer[0] = money >= 5500 ? (int) money / 5500 : 0;
+        answer[1] = money >= 5500 ? (int) money % 5500 : money;
+        
+        return answer;
+    }
+}
